cd2a248593ac49bcf4b1c7a71a0459d5669c535f
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2021-12-12 00:57:48 -0500

    remove header

--- client/src/Screens/Landing.tsx
+++ client/src/Screens/Landing.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../Components/Header";
 import "./Global.css";
 
 function Landing() {
   return (
     <>
       <div style={{ backgroundColor: "red", width: "50", height: "50" }} />
-      <Header />
     </>
   );
 }
