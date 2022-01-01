65050ff56d6b0fb1a768e1f6551a3f965601bfd6
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-01-01 01:03:57 -0500

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Routes>
-        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Homepage />} />
       </Routes>
     </ThemeProvider>
