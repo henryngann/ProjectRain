0fb2ea844268ffb1e6347e0adb1fe2067ee7babd
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-01-08 23:09:26 -0500

    fix bug,checkpoint

--- client/src/Components/organisms/SideDrawerHome.tsx
+++ client/src/Components/organisms/SideDrawerHome.tsx
@@ -1,5 +1,4 @@
 import MailIcon from "@mui/icons-material/Mail";
-import React, { useEffect, useState } from "react";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -11,6 +10,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import React from "react";
 
 const drawerWidth = 240;
 
