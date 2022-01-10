39e5dd04d86586d893fefe6300651171bac921df
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-01-10 13:18:46 -0500

    checkpoint

--- client/src/Components/molecules/OSRSItems.tsx
+++ client/src/Components/molecules/OSRSItems.tsx
@@ -1,59 +1,109 @@
 import React, { useEffect, useState } from "react";
-interface itemApi {
-  _items?: [];
+export interface Root {
+  _items: Item[];
+  _links: Links2;
+  _meta: Meta;
 }
 
-interface itemData {
-  buy_limit?: boolean;
-  cost?: number;
-  duplicate?: boolean;
-  equipable?: boolean;
-  equipable_by_player?: boolean;
-  equipment?: boolean;
-  examine?: string;
-  highalch?: number;
-  icon?: string;
-  id?: string;
-  incomplete?: boolean;
-  last_updated?: string;
-  linked_id_item?: null;
-  linked_id_noted?: null;
+export interface Item {
+  _id: string;
+  id: string;
+  name: string;
+  last_updated: string;
+  incomplete: boolean;
+  members: boolean;
+  tradeable: boolean;
+  tradeable_on_ge: boolean;
+  stackable: boolean;
+  stacked: any;
+  noted: boolean;
+  noteable: boolean;
+  linked_id_item?: number;
+  linked_id_noted?: number;
   linked_id_placeholder?: number;
+  placeholder: boolean;
+  equipable: boolean;
+  equipable_by_player: boolean;
+  equipable_weapon: boolean;
+  cost: number;
   lowalch?: number;
-  members?: boolean;
-  name?: string;
-  noteable?: boolean;
-  noted?: boolean;
-  placeholder?: boolean;
-  quest_item?: boolean;
-  release_date?: string;
-  stackable?: boolean;
-  stacked?: boolean;
-  tradeable?: boolean;
-  tradeable_on_ge?: boolean;
-  weapon?: boolean;
-  weight?: number;
-  wiki_name?: string;
-  wiki_url?: string;
-  _created?: string;
-  _etag?: string;
-  _id?: string;
-  _links?: {};
-  _updated?: string;
+  highalch?: number;
+  weight: number;
+  buy_limit?: number;
+  quest_item: boolean;
+  release_date: string;
+  duplicate: boolean;
+  examine: string;
+  icon: string;
+  wiki_name: string;
+  wiki_url: string;
+  equipment: any;
+  weapon: any;
+  _created: string;
+  _updated: string;
+  _etag: string;
+  _links: Links;
+}
+
+export interface Links {
+  self: Self;
+}
+
+export interface Self {
+  title: string;
+  href: string;
+}
+
+export interface Links2 {
+  parent: Parent;
+  self: Self2;
+  next: Next;
+  last: Last;
+}
+
+export interface Parent {
+  title: string;
+  href: string;
+}
+
+export interface Self2 {
+  title: string;
+  href: string;
+}
+
+export interface Next {
+  title: string;
+  href: string;
+}
+
+export interface Last {
+  title: string;
+  href: string;
+}
+
+export interface Meta {
+  page: number;
+  max_results: number;
+  total: number;
 }
 
 const OSRSItems = () => {
-  const [result, setResult] = useState([]);
+  const [result, setResult] = useState<Item[]>([]);
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
+  const url = "https://api.osrsbox.com/items";
+
+  const doCall = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => result as Item[]);
+
   useEffect(() => {
-    const doCall = () =>
-      fetch("https://api.osrsbox.com/items").then((res) => res.json());
     doCall().then(
-      (res) => {
+      (result) => {
         setIsLoading(false);
-        setResult(res._items);
+        setResult(result);
       },
       (e) => {
         setIsLoading(false);
@@ -61,18 +111,14 @@ const OSRSItems = () => {
       }
     );
   }, []);
+
   console.log(result);
+  console.log(result[0]?.cost);
   return (
     <>
       {isLoading && <p>Loading data...</p>}
       {error && <p>An error occurred</p>}
-      {result && (
-        <>
-          {result.map((item, it) => {
-            return <div key={it}></div>;
-          })}
-        </>
-      )}
+      {result && <>{result[0]?.cost}</>}
     </>
   );
 };
