834929fe7a103c7403c9c6268b05e1f5d13d2ea9
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-01-10 11:28:45 -0500

    add api interface

--- client/src/Components/molecules/OSRSItems.tsx
+++ client/src/Components/molecules/OSRSItems.tsx
@@ -1,7 +1,49 @@
 import React, { useEffect, useState } from "react";
+interface itemApi {
+  _items?: [];
+}
+
+interface itemData {
+  buy_limit?: boolean;
+  cost?: number;
+  duplicate?: boolean;
+  equipable?: boolean;
+  equipable_by_player?: boolean;
+  equipment?: boolean;
+  examine?: string;
+  highalch?: number;
+  icon?: string;
+  id?: string;
+  incomplete?: boolean;
+  last_updated?: string;
+  linked_id_item?: null;
+  linked_id_noted?: null;
+  linked_id_placeholder?: number;
+  lowalch?: number;
+  members?: boolean;
+  name?: string;
+  noteable?: boolean;
+  noted?: boolean;
+  placeholder?: boolean;
+  quest_item?: boolean;
+  release_date?: string;
+  stackable?: boolean;
+  stacked?: boolean;
+  tradeable?: boolean;
+  tradeable_on_ge?: boolean;
+  weapon?: boolean;
+  weight?: number;
+  wiki_name?: string;
+  wiki_url?: string;
+  _created?: string;
+  _etag?: string;
+  _id?: string;
+  _links?: {};
+  _updated?: string;
+}
 
 const OSRSItems = () => {
-  const [result, setResult] = useState({});
+  const [result, setResult] = useState([]);
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -11,7 +53,7 @@ const OSRSItems = () => {
     doCall().then(
       (res) => {
         setIsLoading(false);
-        setResult(res);
+        setResult(res._items);
       },
       (e) => {
         setIsLoading(false);
@@ -24,7 +66,13 @@ const OSRSItems = () => {
     <>
       {isLoading && <p>Loading data...</p>}
       {error && <p>An error occurred</p>}
-      {result && <div>hi</div>}
+      {result && (
+        <>
+          {result.map((item, it) => {
+            return <div key={it}></div>;
+          })}
+        </>
+      )}
     </>
   );
 };
