e00e9fd3357f97d7d733b9e64a897cbbb184f7bf
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-01-29 16:40:54 -0500

    skeleton contact form

--- client/src/Components/molecules/FormCard.tsx
+++ client/src/Components/molecules/FormCard.tsx
@@ -1,59 +1,46 @@
-import * as React from "react";
-import { useTheme } from "@mui/material/styles";
+import { Button, Stack, TextField } from "@mui/material";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
+import * as React from "react";
 
 export default function MediaControlCard() {
-  const theme = useTheme();
-
   return (
-    <Card sx={{ display: "flex" }}>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+    <Card sx={{ display: "flex", width: "50rem", height: "20rem" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="text.secondary"
-            component="div"
+          <Stack
+            direction="column"
+            spacing={3}
+            alignContent="flex-start"
+            justifyContent="flex-start"
           >
-            Mac Miller
-          </Typography>
+            <Stack direction="row" spacing={8}>
+              <TextField id="outlined-basic" label="Name" variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                label="Subject"
+                variant="outlined"
+              />
+            </Stack>
+            <TextField
+              sx={{ width: "48rem" }}
+              id="filled-multiline-flexible"
+              label="Multiline"
+              rows={5}
+              multiline
+            />
+            <Button variant="outlined" sx={{ width: 100, height: 35 }}>
+              Submit
+            </Button>
+          </Stack>
         </CardContent>
-        <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous">
-            {theme.direction === "rtl" ? (
-              <SkipNextIcon />
-            ) : (
-              <SkipPreviousIcon />
-            )}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === "rtl" ? (
-              <SkipPreviousIcon />
-            ) : (
-              <SkipNextIcon />
-            )}
-          </IconButton>
-        </Box>
       </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      />
     </Card>
   );
 }
